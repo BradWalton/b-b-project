91b5e789dbb6fb572c5993c4dfc5594d21886b56
BradWalton <walton4747@gmail.com>
2016-03-14 22:11:33 -0300

    working on signup

--- main.js
+++ main.js
@@ -4,15 +4,15 @@
 //Sign up Scripts
 
 function validate(){
-    var firstName = document.getElementById(firstname).value;
-    var lastName = document.getElementById(lastname).value;
-    var userName = document.getElementById(username).value;
-    var email = document.getElementById(email).value;
-    var password = document.getElementById(password).value;
-    var confirmPassword = document.getElementById(confirmpassword).value;
+    var firstName = document.getElementById(firstName);
+    var lastName = document.getElementById(lastName);
+    var userName = document.getElementById(userName);
+    var email = document.getElementById(email);
+    var password = document.getElementById(password);
+    var confirmPassword = document.getElementById(confirmPassword);
 
     if (firstName === "brad"){
-        console.log("Hey Brad")
+        console.log("Hey Brad");
     }
     else{
 
