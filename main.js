827a8a930af6d12a0dc66b38d5f5a4673ae9042e
BradWalton <walton4747@gmail.com>
2016-03-15 18:26:26 -0300

    restructured form

--- main.js
+++ main.js
@@ -3,20 +3,17 @@
  */
 //Sign up Scripts
 
-function validate(){
-    var firstName = document.getElementById(firstName);
-    var lastName = document.getElementById(lastName);
-    var userName = document.getElementById(userName);
+validate = function(){
+    var fname = document.getElementById(fname);
+    var lname = document.getElementById(lname);
+    var userName = document.getElementById(username);
     var email = document.getElementById(email);
     var password = document.getElementById(password);
-    var confirmPassword = document.getElementById(confirmPassword);
 
-    if (firstName === "brad"){
+    if (fname === "brad"){
         console.log("Hey Brad");
     }
     else{
 
     }
 }
-
-validate();
