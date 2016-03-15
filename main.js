98db507a243e8fff83500b5b900892d0aa123217
BradWalton <walton4747@gmail.com>
2016-03-14 21:01:45 -0300

    set up variables for validate function

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 /**
  * Created by Brad on 2016-03-14.
  */
-console.log("Hello world!")
+//Sign up Scripts
+
+function validate(){
+    var firstName = document.getElementById(firstname).value;
+    var lastName = document.getElementById(lastname).value;
+    var userName = document.getElementById(username).value;
+    var email = document.getElementById(email).value;
+    var password = document.getElementById(password).value;
+    var confirmPassword = document.getElementById(confirmpassword).value;
+}
